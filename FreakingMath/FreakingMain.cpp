f829a2d45651bebce385a1d4838297916a204740
florusp <31383393+florusp@users.noreply.github.com>
2018-03-27 21:06:16 +0700

    Update FreakingMain.cpp

--- FreakingMath/FreakingMain.cpp
+++ FreakingMath/FreakingMain.cpp
@@ -1,9 +1,12 @@
 #include<iostream>
-#include<stdlib.h>
-#include<time.h>
+#include<cstdlib>
+#include<ctime>
 #include<conio.h>
 #include<Windows.h>
 #include<stdio.h>
+#include<fstream>
+#include<string>
+#include<vector>
 
 using namespace std;
 
@@ -11,20 +14,30 @@ int flag;
 int timeCounter = 2;
 int level = 1;
 int _1stNum,_2ndNum;
-int checkOperator;
 int result;
+int randomResult;
+int checkOperator;
+int score = 0;
+int range = 3;
+int Best_Score;
 char Operator;
+bool check;
+bool ans;
 
 void instruction(int &flag);
 void delay(int ms);
-void randomThe_1stNumber(int level, int &num);
-void randomThe_2ndNumber(int level, int &num);
-void randomOperator(int &checkOperator);
-void resultRandom(int num1, int num2, int checkOperator, char &Operator,  int &result);
-void TheQuestion(int num1, int num2, int checkOperator, char Operator, int result);
-void TheAnswer(int &flag);
+void randomThe_1stNumber(int &num, int &range);
+void randomThe_2ndNumber(int &num, int &num1, int &range);
+void randomOperator(int &checkOperator, int &level);
+void resultRandom(int num1, int num2, int checkOperator, char &Operator,  int &result, int &randomResult);
+void TheQuestion(int num1, int num2, int checkOperator, char Operator, int &result, int &randomResult);
+void TheAnswer(bool &ans, int &flag);
+void checkTheAnswer(bool &ans, bool &check, int &flag, int &result, int &randomResult);
 void option(int &flag);
-
+void Score(int flag, int &score);
+void addBestScore(int &Best_Score);
+void BestScore(int &Best_Score);
+void updateLevel(int &score, int &level, int &range);
 
 int main()
 {
@@ -33,8 +46,10 @@ int main()
 	{
 		while (flag == 1)
 		{
-			TheQuestion(_1stNum, _2ndNum, checkOperator, Operator, result);
-			TheAnswer(flag);
+			TheQuestion(_1stNum, _2ndNum, checkOperator, Operator, result, randomResult);
+			TheAnswer(ans,flag);
+			Score(flag,score);
+			updateLevel(score, level, range);
 			timeCounter = 2;
 		}
 		option(flag);
@@ -44,13 +59,24 @@ int main()
 
 void instruction(int &flag)
 {
-	cout << "Welcome to Freaking Math. Press 1 to play or 2 to quit: ";
+	ifstream intro("Instruction.txt");
+	string word;
+	char c;
+	
+	while (!intro.eof())
+	{
+		c = intro.get();
+		word = word + c;
+	}
+	cout << word;
+	intro.close();
 	switch (_getch())
 	{
-	case 49:
+	case 32:
 		flag = 1;
 		break;
-	case 50:
+	case 27:
+		exit(0);
 		flag = 2;
 		break;
 	}
@@ -58,7 +84,7 @@ void instruction(int &flag)
 
 void option(int &flag)
 {
-	cout << "\n\nDo you want to play again? Press 1 to play or 2 to quit: ";
+	cout << "\n\nDo you want to play again? Press 1 to continue or 2 to quit: ";
 	switch (_getch())
 	{
 	case 49:
@@ -76,7 +102,121 @@ void delay(int ms)
 	while (timeDelay > clock());
 }
 
-void TheAnswer(int &flag)
+void randomOperator(int &checkOperator, int &level)
+{
+	if (level % 2 == 1)
+		checkOperator = rand() % 1 + 1;
+	else
+		checkOperator = rand() % 2 + 1;
+}
+
+void randomThe_1stNumber(int &_1stNum, int &range)
+{
+	_1stNum = rand() %  range + 1;
+}
+
+void randomThe_2ndNumber(int &_2ndNum, int &_1stNum, int &range)
+{
+	if (checkOperator == 1)
+		_2ndNum = rand() % range + 1;
+	else
+		_2ndNum = rand() % _1stNum + 1;
+}
+
+
+void resultRandom(int _1stNum, int _2ndNum, int checkOperator, char &Operator, int &result, int &randomResult)
+{
+	switch (checkOperator)
+	{
+		case 1:
+			Operator = 43;
+			result = _1stNum + _2ndNum;
+			randomResult = rand() % 5 + (result - 2); // 5 la so gia tri co the co cua randomResult 
+			break;
+		case 2:
+			Operator = 45;
+			result = _1stNum - _2ndNum;
+			randomResult = rand() % 3 + result; 
+			break;
+	}
+}
+
+void TheQuestion(int _1stNum, int _2ndNum, int checkOperator, char Operator, int &result, int &randomResult)
+{
+	srand((unsigned int)time(NULL));
+	randomOperator(checkOperator, level);
+	randomThe_1stNumber(_1stNum, range);
+	randomThe_2ndNumber(_2ndNum, _1stNum, range);
+	resultRandom(_1stNum, _2ndNum, checkOperator, Operator, result, randomResult);
+	cout << "\n\nIs " << _1stNum << Operator << _2ndNum << "=" << randomResult << " correct? ";
+}
+
+void BestScore(int &Best_Score)
+{
+	ifstream readBS("Best Score.txt");
+	readBS >> Best_Score;
+	readBS.close();
+}
+
+void addBestScore(int &Best_Score)
+{
+	ofstream add("Best Score.txt");
+	add << Best_Score;
+	add.close();
+}
+
+
+void Score(int flag, int &score)
+{
+	if (flag == 1)
+	{
+		score++;
+		cout << "    | Score: " << score;
+	}
+	else
+	{
+		BestScore(Best_Score);
+		if (score >= Best_Score)
+		{
+			Best_Score = score;
+			addBestScore(Best_Score);
+		}
+		cout << "\n\nYou've got: " << score << " | Best Score: " << Best_Score;
+		score = 0;
+	}
+}
+
+void updateLevel(int &score, int &level, int &range)
+{
+	if (score % 10 == 0)
+	{
+		level++;
+		if (score % 20 == 0)
+			range = 10;
+	}
+	else
+		range++;
+}
+void checkTheAnswer(bool &ans, bool &check, int &flag, int &result, int &randomResult)
+{
+	if (result == randomResult)
+		check = true;
+	else
+		check = false;
+
+	if (ans == check)
+	{
+		flag = 1;
+		cout << "TRUE";
+	}
+	else
+	{
+		flag = 2;
+		cout << "FALSE";
+	}
+}
+
+void TheAnswer(bool &ans, int &flag)
 {
 	while (timeCounter != 0)
 	{
@@ -85,12 +225,14 @@ void TheAnswer(int &flag)
 			switch (_getch())
 			{
 			case 49:
-				cout << "You're right";
-				flag = 1;
+				ans = true;
+				cout << "Yes | ";
+				checkTheAnswer(ans, check, flag, result, randomResult);
 				break;
 			case 50:
-				cout << "You're wrong";
-				flag = 2;
+				ans = false;
+				cout << "No | ";
+				checkTheAnswer(ans, check, flag, result, randomResult);
 				break;
 			default:
 				break;
@@ -110,50 +252,4 @@ void TheAnswer(int &flag)
 				delay(1000);
 		}
 	}
-}
-
-void randomThe_1stNumber(int level,int &_1stNum)
-{
-	srand(time(NULL));
-	_1stNum = rand() % level + 1;
-}
-
-void randomThe_2ndNumber(int level,int &_2ndNum)
-{
-	srand(time(NULL));
-	_2ndNum = rand() % level + 1;
-}
-
-void randomOperator(int &checkOperator)
-{
-	srand(time(NULL));
-	checkOperator = rand() % 2 + 1;
-}
-
-void resultRandom(int _1stNum, int _2ndNum, int checkOperator, char &Operator, int &result)
-{
-	switch (checkOperator)
-	{
-		case 1:
-			Operator = 43;
-			result = _1stNum + _2ndNum;
-			srand(time(NULL));
-			//result = rand() % (result - 2) + (result + 2);
-			break;
-		case 2:
-			Operator = 45;
-			result = _1stNum - _2ndNum;
-			srand(time(NULL));
-			//result = rand() % (result - 2) + (result + 2);
-			break;
-	}
-}
-
-void TheQuestion(int _1stNum, int _2ndNum, int checkOperator, char Operator, int result)
-{
-	randomThe_1stNumber(level, _1stNum);
-	randomThe_2ndNumber(level, _2ndNum);
-	randomOperator(checkOperator);
-	resultRandom(_1stNum, _2ndNum, checkOperator, Operator, result);
-	cout << "\n\nIs " << _1stNum << Operator << _2ndNum << "=" << result << " correct? ";
 }
