f46aedf258ba9897e77d425621df21c4bd56b2ff
florusp <31383393+florusp@users.noreply.github.com>
2018-05-28 15:02:26 +0700

    Delete

--- FreakingMath (Graphic Version)/FreakingMath (Graphic Version)/FreakingMath/IntroScreen.h
+++
@@ -1,9 +0,0 @@
-#include"SwitchScreen.h"
-#include<iostream>
-/*man hinh bat dau tro choi*/
-class Intro : public Screen
-{
-	public:
-		// run intro screen
-		virtual int Run(sf::RenderWindow &window);
-};
